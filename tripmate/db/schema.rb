8b269ac5132c3f4c2c9df8503b245e0d73fdb938
TaihakuTsuchiya <tofo.ri.haku.kyoi@gmail.com>
2015-09-08 01:36:33 +0900

    Add unique restriction to email column

--- tripmate/db/schema.rb
+++ tripmate/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907162553) do
+ActiveRecord::Schema.define(version: 20150907163533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,5 +27,7 @@ ActiveRecord::Schema.define(version: 20150907162553) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
