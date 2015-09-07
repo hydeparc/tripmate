67e8d5d8fd8b1a927b93bbe91e281f636adc76e9
TaihakuTsuchiya <tofo.ri.haku.kyoi@gmail.com>
2015-09-08 01:30:36 +0900

    Add null restriction and run 'rake db:migrate' - please execute migration

--- tripmate/db/migrate/20150907162553_create_users.rb
+++ tripmate/db/migrate/20150907162553_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
+      t.string :name, null: false
+      t.string :email, null: false, unique: true
+      t.string :password_digest, null: false
       t.string :photo_path
-      t.string :sex
-      t.datetime :birthday
-      t.string :nationality
+      t.string :sex, null: false
+      t.datetime :birthday, null: false
+      t.string :nationality, null: false
 
       t.timestamps
     end
