0a58d447d71444d28d6f6cdedae75bee935e1f62
TaihakuTsuchiya <tofo.ri.haku.kyoi@gmail.com>
2015-09-12 16:49:57 +0900

    Add format restriction for address

--- tripmate/app/models/user.rb
+++ tripmate/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+  MIN_PASSWORD_LENGTH = 6
+  validates :password, length: { minimum: MIN_PASSWORD_LENGTH }
 end
