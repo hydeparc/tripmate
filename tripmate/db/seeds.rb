868e4bdc52aeb1660db99693379ae00c790c8adb
TaihakuTsuchiya <tofo.ri.haku.kyoi@gmail.com>
2015-09-20 15:43:21 +0900

    Add seed file - plz run 'rake db:seeds'

--- tripmate/db/seeds.rb
+++ tripmate/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |n|
+   User.create(name: "user#{n}", email: "email#{n}@tripmate.com",
+               password: "password", password_confirmation: "password",
+               photo_path: "../public/profile.png", sex: 0,
+               birthday: "1991-01-01", nationality: "Japan")
+end
